0c537e71fdaed38fd41b70dab1590f1b95ea75d5
adarshkumar790 <adarshnit420@gmail.com>
2025-07-18 13:05:26 +0530

    black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <>
-      <h1 className="text-xl font-bold mb-4">My Page with Score Widget</h1>
+      <h1 className="text-xl font-bold mb-4 bg-black">My Page with Score Widget</h1>
 
       {/* Widget container with class used in global CSS */}
-      <div
+      <div color='black'
         id="score-widget"
         className="cdorg-matches"
         data-type="international"
